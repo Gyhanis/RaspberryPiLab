493d83c92d1dc0ec764209abdddb23f3e141829d
gyhanis <2372899031@qq.com>
2019-12-25 14:05:30 +0800

    forwarding & lookup finished

--- Homework/forwarding/forwarding.cpp
+++ Homework/forwarding/forwarding.cpp
@@ -10,7 +10,50 @@
  * @param len 即 packet 的长度，单位为字节
  * @return 校验和无误则返回 true ，有误则返回 false
  */
+
+bool validateIPChecksum(uint8_t *packet) {
+  uint32_t sum,carry,sum2;
+  size_t len;
+  sum = 0;
+  len = packet[0];
+  len &= 0x0f;
+  len <<= 2;
+  for(int i = 0; i < len; i+=2){
+    uint32_t temp;
+    temp = packet[i];
+    temp <<= 8;
+    temp += packet[i+1];
+    sum += temp;
+  }
+  sum2 = sum;
+  while(carry = (sum>>16)){
+    sum &= 0xffff;
+    sum += carry;
+  }
+  if(sum == 0xffff)
+    return true;
+  else{
+    uint32_t temp;
+    temp = packet[10];
+    temp <<= 8;
+    temp |= packet[11];
+    sum2 -= temp;
+    while(carry = (sum2>>16)){
+      sum2 &= 0xffff;
+      sum2 += carry;
+    }
+    temp = ~sum2;
+    packet[11] = temp;
+    packet[10] = temp>>8;
+    return false;
+  }
+}
+
 bool forward(uint8_t *packet, size_t len) {
-  // TODO:
+  if(validateIPChecksum(packet)){
+    packet[8]--;
+    validateIPChecksum(packet);
+    return true;
+  }
   return false;
 }
--- Homework/lookup/lookup.cpp
+++ Homework/lookup/lookup.cpp
@@ -17,6 +17,20 @@
   当 nexthop 为零时这是一条直连路由。
   你可以在全局变量中把路由表以一定的数据结构格式保存下来。
 */
+#define LIST_LEN 128
+RoutingTableEntry entrys[128];
+int cur_len;
+uint32_t MaskList[33] = {
+  0x00000000,
+  0x00000080,0x000000c0,0x000000e0,0x000000f0,
+  0x000000f8,0x000000fc,0x000000fe,0x000000ff,
+  0x000080ff,0x0000c0ff,0x0000e0ff,0x0000f0ff,
+  0x0000f8ff,0x0000fcff,0x0000feff,0x0000ffff,
+  0x0080ffff,0x00c0ffff,0x00e0ffff,0x00f0ffff,
+  0x00f8ffff,0x00fcffff,0x00feffff,0x00ffffff,
+  0x80ffffff,0xc0ffffff,0xe0ffffff,0xf0ffffff,
+  0xf8ffffff,0xfcffffff,0xfeffffff,0xffffffff
+};
 
 /**
  * @brief 插入/删除一条路由表表项
@@ -28,6 +42,31 @@
  */
 void update(bool insert, RoutingTableEntry entry) {
   // TODO:
+  if(insert){
+    for(int i = 0; i < cur_len; i++){
+      if(entrys[i].addr == entry.addr && entrys[i].len == entry.len){
+        entrys[i] = entry;
+        return;
+      }
+    }
+    if(cur_len < LIST_LEN){
+      entrys[cur_len] = entry;
+      cur_len++;
+    }
+  }else{
+    int i,j;
+    i = cur_len - 1;
+    if(i < 0)
+      return;
+    for(j = 0; j < cur_len; j++){
+      if(entrys[j].addr == entry.addr && entrys[j].len == entry.len){
+        entrys[j] = entrys[i];
+        entrys[i].addr = 0;
+        cur_len--;
+        return;
+      }
+    }
+  }
 }
 
 /**
@@ -39,7 +78,15 @@ void update(bool insert, RoutingTableEntry entry) {
  */
 bool query(uint32_t addr, uint32_t *nexthop, uint32_t *if_index) {
   // TODO:
+  int maxlen = 0;
   *nexthop = 0;
   *if_index = 0;
-  return false;
+  for(int i = 0; i < cur_len; i++){
+    if(maxlen < entrys[i].len && ((entrys[i].addr & MaskList[entrys[i].len]) == (addr & MaskList[entrys[i].len]))){
+      *nexthop = entrys[i].nexthop;
+      *if_index = entrys[i].if_index;
+      maxlen = entrys[i].len;
+    }
+  }
+  return (maxlen)?true:false;
 }
